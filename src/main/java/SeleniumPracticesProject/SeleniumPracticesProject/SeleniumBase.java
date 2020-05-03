44b59e9c958cffa9029b49a342a73f3d8c69bfae
shanajPerven <420sorna@gmail.com>
2020-05-03 18:18:21 -0400

    SeleniumBase

---
+++ src/main/java/SeleniumPracticesProject/SeleniumPracticesProject/SeleniumBase.java
@@ -0,0 +1,11 @@
+package SeleniumPracticesProject.SeleniumPracticesProject;
+
+public class SeleniumBase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Selenium");
+
+	}
+
+}
