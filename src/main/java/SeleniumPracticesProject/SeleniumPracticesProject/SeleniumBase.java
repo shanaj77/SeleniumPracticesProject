d8cb29a8b7a13caed8a95ce0a12f6d20e17350c7
shanajPerven <420sorna@gmail.com>
2020-05-03 19:01:52 -0400

    addd

--- src/main/java/SeleniumPracticesProject/SeleniumPracticesProject/SeleniumBase.java
+++ src/main/java/SeleniumPracticesProject/SeleniumPracticesProject/SeleniumBase.java
@@ -6,6 +6,7 @@ public class SeleniumBase {
 		// TODO Auto-generated method stub
 		System.out.println("Selenium");
 		System.out.println("Modified file");
+		System.out.println("Modified file");
 
 	}
 
